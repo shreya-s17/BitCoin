8c9b6424756a4c50875e2463b7b04340f59b9ad4
Shreya <shreya171995@gmail.com>
2018-11-02 14:59:33 -0400

    initialize

---
+++ lib/bitcoin.ex
@@ -0,0 +1,18 @@
+defmodule BITCOIN do
+  @moduledoc """
+  Documentation for BITCOIN.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BITCOIN.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
